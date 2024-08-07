e59d51ff5a488730ac2810c031aaecdb44f205e5
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2024-08-07 12:49:43 +0100

    Update page.tsx
    Signed-off-by: Osinachi Chukwujama  <Osinachi.Chukwujama@gmail.com>

--- src/app/dashboard/(admin)/admin/(overview)/page.tsx
+++ src/app/dashboard/(admin)/admin/(overview)/page.tsx
@@ -3,7 +3,7 @@ import AdminPage from "~/app/dashboard/(admin)/admin/(overview)/client";
 export const metadata = {
   title: "Dashboard",
   description:
-    "Super Admin Dashboard using ShadCN UI components, adhering to design specifications and best practices for accessibility.",
+    "Super Admin Dashboard using ShadCN UI components, Wo, wetin concine me? adhering to design specifications and best practices for accessibility.",
 };
 
 const Admin = () => {
